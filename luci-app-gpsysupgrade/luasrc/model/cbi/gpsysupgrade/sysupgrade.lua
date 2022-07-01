f7be4c2f4944ecd97870473938a9487df3b0ae4e
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-07-01 19:08:16 +0800

    Update sysupgrade.lua

--- luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
+++ luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
@@ -31,6 +31,7 @@ function to_check()
 	sysverformat = luci.sys.exec("date -d $(echo " ..get_system_version().. " | awk -F. '{printf $3\"-\"$1\"-\"$2}') +%s")
 	currentTimeStamp = luci.sys.exec("expr $(date -d \"$(date '+%Y-%m-%d %H:%M:%S')\" +%s) - 172800")
 	if board_name == "x86_64" then
+		model = "x86_64"
 		check_update()
 		if fs.access("/sys/firmware/efi") then
 			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..remote_version.. "-openwrt-x86-64-generic-squashfs-combined-efi.img.gz"
