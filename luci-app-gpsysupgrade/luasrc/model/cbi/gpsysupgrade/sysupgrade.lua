9269c8b54c99704835405edb5a8b6c547a6943d9
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-27 16:26:38 +0800

    Update sysupgrade.lua

--- luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
+++ luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
@@ -96,6 +96,10 @@ function to_check()
 		model = "rockchip_armv8/xunlong_orangepi-r1-plus"
 		check_update()
 		download_url = "https://op.supes.top/firmware/" ..model.. "/" ..remote_version.. "-openwrt-rockchip-armv8-xunlong_orangepi-r1-plus-squashfs-sysupgrade.img.gz"
+    elseif board_name:match("ariaboard,photonicat$") then
+		model = "rockchip_armv8/ariaboard_photonicat"
+		check_update()
+		download_url = "https://op.supes.top/firmware/" ..model.. "/" ..remote_version.. "-openwrt-rockchip-armv8-ariaboard_photonicat-squashfs-sysupgrade.img.gz"
     elseif board_name:match("nanopi%-neo3$") then
 		model = "rockchip_armv8/friendlyarm_nanopi-neo3"
 		check_update()
